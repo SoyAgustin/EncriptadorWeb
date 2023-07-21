65ca1e0eb2d9c62f80e6a0834edc5f8a3cb97aaf
SoyAgustin <Agustinphysx@gmail.com>
2023-07-21 13:55:39 -0600

    Agregada función de desencriptado, logica del challenge completa

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+//Diccionario de encriptacion
 let encryptDict = {"a": "ai","e":"enter","i":"imes","o":"ober","u":"ufat"};
 
+//Diccionario de desencriptacion (orden inverso entre llave y valor del diccionario de encriptacion)
 let decryptDict = {};
 for (let key in encryptDict){
     let value = encryptDict[key];
     decryptDict[value] = key;
 }
 
+//Funcion para encryptar texto usando ciclos for anidados
 function encryptText(text){
+    
     const vocals = ["a","e","i","o","u"];
     let characterList = text.split("");
-    
+
     for(indexA in characterList){
         for (indexB in vocals){
 
@@ -17,14 +21,27 @@ function encryptText(text){
             let letter = vocals[indexB];
 
             if(character==letter){
-                //console.log(encryptDict[letter]);
                 characterList[indexA] = encryptDict[letter];
             }
         }
     }
 
     text = characterList.join("");
-    //console.log(text);
+    return text;
+}
+
+//Funcion para desencryptar texto usando expresiones regulares (regex)
+//Las keys de decryptDict se usan como expresiones regulares para reemplazarlas por su valor
+function decryptText(text){
+
+    for (let key in decryptDict){
+
+        let value = decryptDict[key];
+        let regex = new RegExp(key, "g");
+
+        text = text.replace(regex, value);
+    }
+
     return text;
 }
 
